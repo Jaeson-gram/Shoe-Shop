f2acb8ca2a04c214c92e97ab9b8826177cec30d3
Jaeson-gram <freeonjeyson@gmail.com>
2024-12-03 08:17:30 +0100

    initial commit

---
+++ lib/global_variables.dart
@@ -0,0 +1,45 @@
+final products = [
+  {
+    'id': '0',
+    'title': 'Men\'s Nike',
+    'price': 45.77,
+    'sizes': [8, 10, 12],
+    'imgURL': 'assets/images/shoe_1.png',
+    'company': 'Nike'
+  },
+  {
+    'id': '1',
+    'title': 'Women\'s Bata',
+    'price': 45.77,
+    'sizes': [8, 10, 12],
+    'imgURL': 'assets/images/shoe_3.png',
+    'company': 'Nike'
+  },
+  {
+    'id': '2',
+    'title': 'Sneakers by Adidas',
+    'price': 45.77,
+    'sizes': [8, 10, 12],
+    'imgURL': 'assets/images/shoe_4.png',
+    'company': 'Adidas'
+  },
+  {
+    'id': '3',
+    'title': 'Women\'s Nike',
+    'price': 45.77,
+    'sizes': [8, 10, 12],
+    'imgURL': 'assets/images/shoe_2.png',
+    'company': 'Nike'
+  },
+];
+
+final cart = [
+  {
+    'id': 3,
+    'title': 'Jordan Shoes',
+    'price': 430.98,
+    'size': 8,
+    'imgURL': 'assets/images/shoe_4.jpg',
+    'company': 'Jordan'
+  }
+];
